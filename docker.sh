5af0af443e3d0aa55b68fc18d2cd84407a0b6986
Sitnikov333 <44017329+Sitnikov333@users.noreply.github.com>
2021-07-06 13:53:38 +0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,1 +1,5 @@
-111
+#!/bin/bash
+curl -s https://raw.githubusercontent.com/Sitnikov333/devops_course/main/add/Dockerfile > Dockerfile
+curl -s https://raw.githubusercontent.com/Sitnikov333/devops_course/main/add/index.html > index.html
+docker build -t my-site:latest .
+docker run -p 80:80 -d my-site
